600881c10643992751e1bd8004e4fe694dc79181
jessica-lau <jessica.lau2@gmail.com>
2020-05-20 17:27:18 -0400

    add jquery effects

--- js/magic-8-ball.js
+++ js/magic-8-ball.js
@@ -13,19 +13,28 @@
 $(document).ready(function() {
 var magic8Ball = {};
 magic8Ball.listOfAnswers = ["Most likely", "Yes, definitely", "Don't count on it", "Cannot predict now", "Ask again later", "Maybe"];
+$("#answer").hide();
+
+//answer function
 magic8Ball.askQuestion = function (question) {
+    $("#8ball").effect("shake");
+    $("#answer").fadeIn(4000);
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
     var randomAnswer = Math.floor(Math.random() * this.listOfAnswers.length);
     var magic8BallAnswer = this.listOfAnswers[randomAnswer];
 
     $("#answer").text(magic8BallAnswer);
-    
+
     console.log(question);
     console.log(magic8BallAnswer);
 };
 
+//question prompt function
 var onClick = function() {
-	var question = prompt("Ask me a question.");
-	magic8Ball.askQuestion(question);
+    $("#answer").hide();
+    $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+	setTimeout( function() {var question = prompt("Ask me a question.");
+	magic8Ball.askQuestion(question);}, 500);
 };
 
 $("#questionButton").click(onClick);
